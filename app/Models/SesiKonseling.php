228c190408a587760814d531e93601edef97903f
wahyudingalang115_arch <wahyudingalang115@gmail.com>
2026-09-21 18:37:23 +0700

    Memperbaiki bentrok di model SesiKonseling

--- app/Models/SesiKonseling.php
+++ app/Models/SesiKonseling.php
@@ -2,34 +2,19 @@
 
 namespace App\Models;
 
-<<<<<<< HEAD
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-=======
-use App\Models\User;
->>>>>>> b5a22db7bb419c461979e3c7192a37bb1143a5b6
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\User;
 
 class SesiKonseling extends Model
 {
-<<<<<<< HEAD
     use HasFactory;
 
-    // Menghubungkan model ke tabel yang sudah ada
+    // Menghubungkan model ke tabel
     protected $table = 'sesi_konselings';
 
     // Kolom yang boleh diisi
-    protected $fillable = [
-        'siswa_id', 
-        'topik', 
-        'jadwal', 
-        'status', 
-        'tipe'              
-    ];
-
-    // Relasi ke tabel users (sebagai siswa)
-    public function siswa()
-=======
     protected $fillable = [
         'siswa_id',
         'topik',
@@ -38,12 +23,13 @@ class SesiKonseling extends Model
         'tipe',
     ];
 
+    // Memastikan jadwal dibaca sebagai format waktu (datetime) otomatis
     protected $casts = [
         'jadwal' => 'datetime',
     ];
 
+    // Relasi ke tabel users (sebagai siswa)
     public function siswa(): BelongsTo
->>>>>>> b5a22db7bb419c461979e3c7192a37bb1143a5b6
     {
         return $this->belongsTo(User::class, 'siswa_id');
     }
